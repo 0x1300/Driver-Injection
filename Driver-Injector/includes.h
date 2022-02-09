2ad387d4b8bb99c0bec0d3018d48697d45155f79
0x1300 <ttlddevelopment@gmail.com>
2022-02-09 18:38:39 +0100

    -Added Includes for all -Updated main.cpp -Updated Project Settings

---
+++ Driver-Injector/includes.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <future>
+#include <Windows.h>
+#include <winternl.h>
+#include <assert.h>
+#include <tchar.h>
+#include <ShlObj_core.h>
+#include <AccCtrl.h>
+#include <AclAPI.h>
+#include <shellapi.h>
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <cstdlib>
+#include <stdlib.h>
+#include <ostream>
+#include <regex>
+#include <fstream>
+#include <filesystem>
+#include <chrono>
+#include "xor.hpp"
+
+#pragma comment(lib, "urlmon.lib")
+#pragma comment(lib,"wininet.lib")
---
+++ Driver-Injector/main.cpp
@@ -0,0 +1,14 @@
+#include "includes.h"
+
+
+void dospoofing() //we do spoofing as first step
+{
+
+}
+
+
+void dodrivermapping() //we do driver mapping next
+{
+
+
+}
